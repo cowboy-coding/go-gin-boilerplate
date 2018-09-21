e9498e1ae73e9ebe815e7d0e3509bf90cbcb55aa
kalle saas <kalle@easypep.de>
2018-09-21 16:07:16 +0200

    add service for Jwt auth

--- lib/services/auth_service.go
+++ lib/services/auth_service.go
@@ -1,9 +1,62 @@
 package services
 
 import (
+	jwt "github.com/appleboy/gin-jwt"
+	"github.com/cowboy-coding/go-gin-boilerplate/forms"
 	"github.com/cowboy-coding/go-gin-boilerplate/models"
+	"github.com/gin-gonic/gin"
 )
 
-func Authenticate(requestUser *models.User) int {
-	return 0
+type AuthService struct{}
+
+var identityKey = "id"
+
+func (u AuthService) PayloadFunc(data interface{}) jwt.MapClaims {
+	if v, ok := data.(*models.User); ok {
+		return jwt.MapClaims{
+			identityKey: v.UserName,
+		}
+	}
+	return jwt.MapClaims{}
+}
+
+func (u AuthService) IdentityHandler(c *gin.Context) interface{} {
+	claims := jwt.ExtractClaims(c)
+	return &models.User{
+		UserName: claims["id"].(string),
+	}
+}
+
+func (u AuthService) Authenticator(c *gin.Context) (interface{}, error) {
+	var loginVals forms.LoginForm
+	if err := c.ShouldBind(&loginVals); err != nil {
+		return "", jwt.ErrMissingLoginValues
+	}
+	userID := loginVals.Username
+	password := loginVals.Password
+
+	if (userID == "admin" && password == "admin") || (userID == "test" && password == "test") {
+		return &models.User{
+			UserName:  userID,
+			LastName:  "Bo-Yi",
+			FirstName: "Wu",
+		}, nil
+	}
+
+	return nil, jwt.ErrFailedAuthentication
+}
+
+func (u AuthService) Authorizator(data interface{}, c *gin.Context) bool {
+	if v, ok := data.(*models.User); ok && v.UserName == "admin" {
+		return true
+	}
+
+	return false
+}
+
+func (u AuthService) Unauthorized(c *gin.Context, code int, message string) {
+	c.JSON(code, gin.H{
+		"code":    code,
+		"message": message,
+	})
 }
--- middlewares/jwt_auth_middleware.go
+++ middlewares/jwt_auth_middleware.go
@@ -5,77 +5,23 @@ import (
 	"time"
 
 	jwt "github.com/appleboy/gin-jwt"
-	"github.com/cowboy-coding/go-gin-boilerplate/config"
-	"github.com/cowboy-coding/go-gin-boilerplate/forms"
-	"github.com/cowboy-coding/go-gin-boilerplate/models"
-	"github.com/gin-gonic/gin"
+	"github.com/cowboy-coding/go-gin-boilerplate/lib/services"
 )
 
-var identityKey = "id"
-
-func payloadFunc(data interface{}) jwt.MapClaims {
-	if v, ok := data.(*models.User); ok {
-		return jwt.MapClaims{
-			identityKey: v.UserName,
-		}
-	}
-	return jwt.MapClaims{}
-}
-
-func identityHandler(c *gin.Context) interface{} {
-	claims := jwt.ExtractClaims(c)
-	return &models.User{
-		UserName: claims["id"].(string),
-	}
-}
-
-func authenticator(c *gin.Context) (interface{}, error) {
-	var loginVals forms.LoginForm
-	if err := c.ShouldBind(&loginVals); err != nil {
-		return "", jwt.ErrMissingLoginValues
-	}
-	userID := loginVals.Username
-	password := loginVals.Password
-
-	if (userID == "admin" && password == "admin") || (userID == "test" && password == "test") {
-		return &models.User{
-			UserName:  userID,
-			LastName:  "Bo-Yi",
-			FirstName: "Wu",
-		}, nil
-	}
-
-	return nil, jwt.ErrFailedAuthentication
-}
-
-func authorizator(data interface{}, c *gin.Context) bool {
-	if v, ok := data.(*models.User); ok && v.UserName == "admin" {
-		return true
-	}
-
-	return false
-}
-
-func unauthorized(c *gin.Context, code int, message string) {
-	c.JSON(code, gin.H{
-		"code":    code,
-		"message": message,
-	})
-}
-
 func JwtAuthMiddleware() *jwt.GinJWTMiddleware {
-	c := config.GetConfig()
+	// later: config := config.GetConfig()
+	authService := new(services.AuthService)
 	authMiddleware, err := jwt.New(&jwt.GinJWTMiddleware{
-		Realm:           c.GetString("jwt.realm"),
-		Key:             []byte(c.GetString("jwt.secret_key")),
+		Realm:           "jwt.realm",
+		Key:             []byte("jwt.secret_key"),
 		Timeout:         time.Hour,
 		MaxRefresh:      time.Hour,
-		IdentityKey:     identityKey,
-		PayloadFunc:     payloadFunc,
-		IdentityHandler: identityHandler,
-		Authenticator:   authenticator,
-		Authorizator:    authorizator,
-		Unauthorized:    unauthorized,
+		IdentityKey:     "id",
+		PayloadFunc:     authService.PayloadFunc,
+		IdentityHandler: authService.IdentityHandler,
+		Authenticator:   authService.Authenticator,
+		Authorizator:    authService.Authorizator,
+		Unauthorized:    authService.Unauthorized,
 		TimeFunc:        time.Now,
 	})
 
